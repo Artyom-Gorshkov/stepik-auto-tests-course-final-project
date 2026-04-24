a5602e95a3a67c28783e13776f7722b53b1a007f
Gorshkov_AL <gorshkov5371@yandex.ru>
2026-04-24 18:06:25 +0300

    Добавлены тесты для страницы авторизации

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None, help="Choose language of browser")
+    parser.addoption('--language', action='store', default='en', help="Choose language of browser")
 
 
 @pytest.fixture(scope="function")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class LoginPage(BasePage):
@@ -8,13 +10,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert 'login' in WebDriver.current_url
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No login form on the page"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "No register form on the page"
